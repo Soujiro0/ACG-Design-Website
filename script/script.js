fd8ccdd77ba598a0f34b13410f57188f0eca0a08
Soujiro0 <castrudesjerry11@gmail.com>
2024-05-19 21:43:14 +0800

    Layout 2.0

--- script/script.js
+++ script/script.js
@@ -5,22 +5,22 @@
  */
 
 // Entry Point
-document.addEventListener('DOMContentLoaded', fetchDataAndRender);
+document.addEventListener('DOMContentLoaded', fetchData);
 
 // Fetching data from JSON file
-async function fetchDataAndRender() {
+async function fetchData() {
 
     try {
         const response = await fetch('/data/data.json');
         const data = await response.json();
 
-        const productListDiv = document.querySelector('.productList');
+        // const productListDiv = document.querySelector('.productList');
         
-        data.forEach(item => {
-            // Create component and append to container
-            const productHTML = createProductHTML(item);
-            productListDiv.appendChild(productHTML);
-        });
+        // data.forEach(item => {
+        //     // Create component and append to container
+        //     const productHTML = createProductHTML(item);
+        //     productListDiv.appendChild(productHTML);
+        // });
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -30,28 +30,28 @@ async function fetchDataAndRender() {
 }
 
 // Create Component
-function createProductHTML(product) {
+function createProductHTML(item) {
 
     // Create elements
     const productDiv = document.createElement('div');
     productDiv.classList.add('product');
-    productDiv.id = 'item' + product.id.toString();
+    productDiv.id = 'item' + item.id.toString();
 
     const img = document.createElement('img');
-    img.setAttribute('src', product.thumbnail);
+    img.setAttribute('src', item.thumbnail);
     img.setAttribute('alt', '');
 
     const productInfoDiv = document.createElement('div');
     productInfoDiv.classList.add('productInfo');
 
     const h1 = document.createElement('h1');
-    h1.textContent = product.name;
+    h1.textContent = item.name;
 
     const h2 = document.createElement('h2');
-    h2.textContent = '$' + product.price.toFixed(2);
+    h2.textContent = '$' + item.price.toFixed(2);
 
     const h3 = document.createElement('h3');
-    h3.textContent = product.category;
+    h3.textContent = item.category;
 
     // Append elements
     productInfoDiv.appendChild(h1);
@@ -62,7 +62,7 @@ function createProductHTML(product) {
     productDiv.appendChild(productInfoDiv);
 
     productDiv.addEventListener('click', () => {
-        itemSelect(product);
+        itemSelect(item);
     });
     return productDiv;
 
@@ -84,11 +84,11 @@ async function filterItems() {
     console.log(categoryFilter.value);
     let filterItems;
     if (categoryFilter.value === 'all') {
-        filterItems = await fetchDataAndRender();
+        filterItems = await fetchData();
         console.log(`filtered items All:`);
         console.log(filterItems);
     } else {
-        const filter = await fetchDataAndRender();
+        const filter = await fetchData();
         filterItems = filter.filter(item => item.category === categoryFilter.value);
         console.log(`filtered items`);
         console.log(filterItems);
